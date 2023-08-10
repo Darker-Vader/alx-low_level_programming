55ed8139ae6c9f2724b01ab8ac9799a1b94f4b86
Darker-Vader <chibems8@gmail.com>
2023-08-10 08:03:20 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,8 @@
  *
  * Return: always 0
  */
-int main(void) {	
+int main(void)
+{
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
 	printf("size of an int: %zu byte(s)\n", sizeof(int));
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
