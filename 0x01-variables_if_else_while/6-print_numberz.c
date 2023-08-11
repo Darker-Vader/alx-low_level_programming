d288116d7da39098cc9fce611f31350aac724361
Darker-Vader <chibems8@gmail.com>
2023-08-11 07:06:02 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - is a function that prints all digit numbers of base10 starting from 0
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0 ; d < 10 ; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
