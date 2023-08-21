7834827cc406b9aa6a406a5131a2ce3cdf536203
Darker-Vader <138698818+Darker-Vader@users.noreply.github.com>
2023-08-21 15:42:28 +0100

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,12 +4,15 @@
  * swap_int - swaps the values of two integers
  * @a: function parameter1
  * @b: function parameter2
- * Return: 0
+ * Return: nothing
 */
 
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
-	_putchar('\n');
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+
 }
